d70a142fcb11e6f47e6f2c1e1af77b795ca8015d
Gustavo <gustavoog05@hotmail.com>
2025-10-23 12:08:18 -0300

    fixing files to install n8n verification

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { task, src, dest } = require('gulp');
 
 task('build:icons', copyIcons);
+task('build:index', copyIndex);
 
 function copyIcons() {
 	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
@@ -13,4 +14,23 @@ function copyIcons() {
 	const credDestination = path.resolve('dist', 'credentials');
 
 	return src(credSource).pipe(dest(credDestination));
+}
+
+function copyIndex() {
+	const indexSource = path.resolve('index.js');
+	const indexDestination = path.resolve('dist');
+	
+	return src(indexSource)
+		.pipe(dest(indexDestination))
+		.on('end', () => {
+			// Ajustar os caminhos no index.js copiado
+			const fs = require('fs');
+			const indexPath = path.resolve('dist', 'index.js');
+			let content = fs.readFileSync(indexPath, 'utf8');
+			
+			// Remover ./dist/ dos requires já que agora estará dentro de dist
+			content = content.replace(/\.\/dist\//g, './');
+			
+			fs.writeFileSync(indexPath, content);
+		});
 }
